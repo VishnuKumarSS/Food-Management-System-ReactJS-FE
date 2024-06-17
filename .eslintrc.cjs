9bd039c143d2cdfce8c8b97be9f2202a2f501b7d
VishnuKumarSS <vishnukumarss941@gmail.com>
2024-06-17 19:47:34 +0530

    Alias fixes and config changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,72 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  plugins: ["react-refresh"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
+    // "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
+
+// TODO: REMOVE THE BELOW
+
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:react/recommended",
+//     "plugin:@typescript-eslint/recommended",
+//     "plugin:import/recommended",
+//     "plugin:import/typescript",
+//     "plugin:jsx-a11y/recommended",
+//     "plugin:prettier/recommended",
+//   ],
+//   parser: "@typescript-eslint/parser",
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 12,
+//     sourceType: "module",
+//   },
+//   plugins: ["react", "@typescript-eslint", "import", "jsx-a11y", "prettier"],
+//   settings: {
+//     react: {
+//       version: "detect",
+//     },
+//     "import/resolver": {
+//       alias: {
+//         map: [
+//           ["@components", "./src/components"],
+//           ["@pages", "./src/pages"],
+//           ["@utils", "./src/utils"],
+//         ],
+//         extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+//       },
+//       typescript: {},
+//     },
+//   },
+//   rules: {
+//     "prettier/prettier": ["error"],
+//     "react/react-in-jsx-scope": "off",
+//     "react/prop-types": "off",
+//     "@typescript-eslint/explicit-module-boundary-types": "off",
+//     "import/no-unresolved": "error",
+//   },
+// };
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,22 @@
+import path from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
+
+// TODO: Remove the below
 // import path from "path";
 // import { defineConfig } from "vite";
 // import { fileURLToPath } from "url";
@@ -17,21 +36,3 @@
 //     },
 //   },
 // });
-
-import path from "path";
-import { defineConfig } from "vite";
-import { fileURLToPath } from "url";
-
-import react from "@vitejs/plugin-react";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
