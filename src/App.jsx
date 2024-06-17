fc587eea2bb7398cb44ccaf6ef2374a10543d784
VishnuKumarSS <vishnukumarss941@gmail.com>
2024-06-17 20:08:02 +0530

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from "@components/ui/button";
+
 function App() {
-  return <div className="text-2xl font-extrabold">Hello world!</div>;
+  return (
+    <div className="text-2xl font-extrabold">
+      Hello world!
+      <Button>Button</Button>
+    </div>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -12,27 +12,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
 });
-
-// TODO: Remove the below
-// import path from "path";
-// import { defineConfig } from "vite";
-// import { fileURLToPath } from "url";
-
-// import react from "@vitejs/plugin-react";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@components": path.resolve(__dirname, "./src/components"),
-//       "@pages": path.resolve(__dirname, "./src/pages"),
-//       "@utils": path.resolve(__dirname, "./src/utils"),
-//     },
-//   },
-// });
