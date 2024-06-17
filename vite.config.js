3b7d011acd68b3d311e0263991d8dafff89d9400
VishnuKumarSS <vishnukumarss941@gmail.com>
2024-06-17 19:18:49 +0530

    Path alias and tailwind setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import path from "path";
+// import { defineConfig } from "vite";
+// import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
+// import react from "@vitejs/plugin-react";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@components": path.resolve(__dirname, "./src/components"),
+//       "@pages": path.resolve(__dirname, "./src/pages"),
+//       "@utils": path.resolve(__dirname, "./src/utils"),
+//     },
+//   },
+// });
+
+import path from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
