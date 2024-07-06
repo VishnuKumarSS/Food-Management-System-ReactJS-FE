52ce16c286e30847a0f89f7d1aa24ae5d9ae6255
VishnuKumarSS <vishnukumarss941@gmail.com>
2024-07-06 18:30:16 +0530

    feat: pagination addition for food items page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,66 @@
 import React, { useState, useEffect } from "react";
 import FoodItem from "@/components/FoodItem";
 import { api } from "@services/api";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 function Home() {
   const [foodItems, setFoodItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [previousPage, setPreviousPage] = useState(null);
+  const [nextPage, setNextPage] = useState(null);
   const token = localStorage.getItem("token");
+  const pageSize = 4;
+
+  const fetchData = async (page, pageSize) => {
+    try {
+      const [foodResponse, cartResponse] = await Promise.all([
+        api.get(`/food/food-items/?page=${page}&page_size=${pageSize}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }),
+        api.get("/orders/cart/", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }),
+      ]);
+
+      const foodData = foodResponse.data;
+      const cartData = cartResponse.data;
+      setPreviousPage(foodData?.previous);
+      setNextPage(foodData?.next);
+
+      // Merge the cart quantities into the food items
+      const itemsWithQuantity = foodData.results.map((item) => {
+        const cartItem = cartData.items.find(
+          (cartItem) => cartItem.food_item.id === item.id
+        );
+        return {
+          ...item,
+          quantity: cartItem ? cartItem.quantity : 0,
+        };
+      });
+
+      setFoodItems(itemsWithQuantity);
+      setTotalPages(Math.ceil(foodData.count / pageSize));
+    } catch (error) {
+      console.error("There was an error fetching the data!", error);
+    }
+  };
 
   useEffect(() => {
-    // Fetch both food items and cart data
-    const fetchData = async () => {
-      try {
-        const [foodResponse, cartResponse] = await Promise.all([
-          api.get("/food/food-items/", {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }),
-          api.get("/orders/cart/", {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }),
-        ]);
-
-        const foodData = foodResponse.data;
-        const cartData = cartResponse.data;
-
-        // Merge the cart quantities into the food items
-        const itemsWithQuantity = foodData.map((item) => {
-          const cartItem = cartData.items.find(
-            (cartItem) => cartItem.food_item.id === item.id
-          );
-          return {
-            ...item,
-            quantity: cartItem ? cartItem.quantity : 0,
-          };
-        });
-
-        setFoodItems(itemsWithQuantity);
-      } catch (error) {
-        console.error("There was an error fetching the data!", error);
-      }
-    };
-
-    fetchData();
-  }, [token]);
+    fetchData(currentPage, pageSize);
+  }, [token, currentPage]);
 
   const handleQuantityChange = (foodItemId, newQuantity, numberOfQuantity) => {
     console.log("Changing quantity for item", foodItemId, "to", newQuantity);
@@ -81,6 +97,39 @@ function Home() {
     handleQuantityChange(foodItemId, currentQuantity - 1, -1);
   };
 
+  const handlePageChange = ({
+    newPage,
+    previousPageClicked = false,
+    nextPageClicked = false,
+  }) => {
+    if (
+      (previousPageClicked === true && previousPage !== null) ||
+      (nextPageClicked === true && nextPage !== null) ||
+      (newPage > 0 && newPage <= totalPages)
+    ) {
+      setCurrentPage(newPage);
+    }
+  };
+
+  const renderPaginationLinks = () => {
+    let links = [];
+    for (let i = 1; i <= totalPages; i++) {
+      links.push(
+        <PaginationItem key={i}>
+          <PaginationLink
+            href="#"
+            isActive={i === currentPage}
+            onClick={() => handlePageChange({ newPage: i })}
+          >
+            {i}
+          </PaginationLink>
+        </PaginationItem>
+      );
+    }
+    console.log("links", links);
+    return links;
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <h1>Food Menu</h1>
@@ -94,6 +143,38 @@ function Home() {
           />
         ))}
       </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              href="#"
+              onClick={() =>
+                handlePageChange({
+                  newPage: currentPage - 1,
+                  previousPageClicked: true,
+                })
+              }
+              disabled={currentPage === 1}
+            />
+          </PaginationItem>
+          {renderPaginationLinks()}
+          {/* <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem> */}
+          <PaginationItem>
+            <PaginationNext
+              href="#"
+              onClick={() =>
+                handlePageChange({
+                  newPage: currentPage + 1,
+                  nextPageClicked: true,
+                })
+              }
+              disabled={currentPage === totalPages}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 }
